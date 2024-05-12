cc06943451b70fbeaaa134c47cb4b96542d4849d
Quentin LAURENT <quentin.laurent.sup@gmail.com>
2024-05-12 20:49:03 +0200

    base implementation for Buildings, Floors and Rooms
    Started implementing the very base of the application: the ability to visualize Buildings, Floors and Rooms.
    As of now, there isn't a dedicated interface to create instances of Buildings, Floors and Rooms (apart from Django Admin of course).
    To create instances of those classes, it is necessary to use either:
    - The Django Admin application
    - The Django shell
    
    This basic implementation also include the base template (built with tailwindcss + Flowbite) that will be used for all future pages.

--- aubouleau_web/models.py
+++ aubouleau_web/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Building(models.Model):
+    name = models.CharField(max_length=64)
+    created_at = models.DateTimeField("Creation timestamp")
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_total_floors(self):
+        return self.floor_set.count()
+
+    def get_total_rooms(self):
+        total_rooms = 0
+        for floor in self.floor_set.all():
+            total_rooms += floor.get_total_rooms()
+        return total_rooms
+
+    def get_all_rooms(self):
+        return Room.objects.filter(floor__building=self.id)
+
+
+class Floor(models.Model):
+    number = models.IntegerField("Floor number")
+    name = models.CharField(max_length=64)
+    created_at = models.DateTimeField("Creation timestamp")
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} (nº{self.number})'
+
+    def get_total_rooms(self):
+        return self.room_set.count()
+
+    def get_all_rooms(self):
+        return self.room_set.all()
+
+
+class Room(models.Model):
+    number = models.CharField(max_length=8)
+    name = models.CharField(max_length=64)
+    created_at = models.DateTimeField("Creation timestamp")
+    floor = models.ForeignKey(Floor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} (nº{self.number})'
--- aubouleau_web/views.py
+++ aubouleau_web/views.py
@@ -1,3 +1,45 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Building, Floor, Room
+
+
+def index(request):
+    return render(request, "aubouleau_web/index.html")
+
+
+def buildings(request):
+    buildings_list = Building.objects.all()
+    return render(request, "aubouleau_web/buildings.html", {"buildings": buildings_list})
+
+
+def building_detail(request, building_id):
+    building = Building.objects.get(pk=building_id)
+    floors_list = building.floor_set.all()
+    rooms_list = building.get_all_rooms()
+    return render(request, "aubouleau_web/building_detail.html", {"building": building, "floors": floors_list, "rooms": rooms_list})
+
+
+def floors(request, building_id):
+    building = Building.objects.get(pk=building_id)
+    floors_list = building.floor_set.all()
+    return render(request, "aubouleau_web/floors.html", {"building": building, "floors": floors_list})
+
+
+def floor_detail(request, building_id, floor_id):
+    building = Building.objects.get(pk=building_id)
+    floor = Floor.objects.get(pk=floor_id)
+    rooms_list = floor.room_set.all()
+    return render(request, "aubouleau_web/floor_detail.html", {"building": building, "floor": floor, "rooms": rooms_list})
+
+
+def rooms(request, building_id):
+    building = Building.objects.get(pk=building_id)
+    rooms_list = building.get_all_rooms()
+    return render(request, "aubouleau_web/rooms.html", {"building": building, "rooms": rooms_list})
+
+
+def room_detail(request, building_id, room_id):
+    building = Building.objects.get(pk=building_id)
+    room = Room.objects.get(pk=room_id)
+    return render(request, "aubouleau_web/room_detail.html", {"building": building, "room": room})
