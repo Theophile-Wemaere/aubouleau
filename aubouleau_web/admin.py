5df55fc31317b4c2ed7777fb45d074d9c0ba358a
Charles Mailley <106654034+Deathvanos@users.noreply.github.com>
2024-05-13 16:06:44 +0200

    added models: Equipment and EquipmentType
    Taken from 22451005f404ebbe39b24ba4837143b02ffaaf6a

--- aubouleau_web/admin.py
+++ aubouleau_web/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Building, Floor, Room
+from .models import *
 
 admin.site.register(Building)
 admin.site.register(Floor)
 admin.site.register(Room)
+
+admin.site.register(EquipmentType)
+admin.site.register(Equipment)
