48a3e7b6ca7bea695116788e62ef461939fbda6a
Quentin LAURENT <quentin.laurent.sup@gmail.com>
2024-05-19 20:02:38 +0200

    added documentation for models and views
    Note: The documentation uses the reStructuredText markup.
    See:
    - https://devguide.python.org/documentation/markup/
    - https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html

--- aubouleau_web/context_processors.py
+++ aubouleau_web/context_processors.py
@@ -3,10 +3,9 @@ from .models import Building, Floor, Room
 
 def add_sidebar_context(request):
     """
-    Adds to the context of all templates the three following attributes:
-        - "sidebar_buildings", which contains a list of all the Buildings
-        - "sidebar_floors", which contains a list of all the Floors
-        - "sidebar_rooms", which contains a list of all the Rooms
+    Adds lists containing all Buildings, Floors and Rooms to the context of all templates.
+    :param request: The HTTP request.
+    :return: A dictionary containing all the context variables required by the sidebar.
     """
     return {
         "sidebar_buildings": Building.objects.all(),
