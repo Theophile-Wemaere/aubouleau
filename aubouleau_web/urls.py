2b55711f85c60e8a8b5500dbfb6940ac39a179f2
Quentin LAURENT <quentin.laurent.sup@gmail.com>
2024-05-19 18:37:35 +0200

    updated URLs examples in urls.py

--- aubouleau_web/urls.py
+++ aubouleau_web/urls.py
@@ -11,19 +11,19 @@ urlpatterns = [
     # /buildings
     # Example: https://aubouleau.fr/buildings
     path("buildings/", views.buildings, name="buildings"),
-    # /[building_id]
-    # Example: https://aubouleau.fr/ndc
+    # /[building_name]
+    # Example: https://aubouleau.fr/NDC
     path("<str:building_name>/", views.building_detail, name="building_detail"),
-    # /[building_id]/floors
-    # Example: https://aubouleau.fr/ndc/floors
+    # /[building_name]/floors
+    # Example: https://aubouleau.fr/NDC/floors
     path("<str:building_name>/floors/", views.floors, name="floors"),
-    # /[building_id]/floors/[floor_id]
-    # Example: https://aubouleau.fr/ndc/floors/0
+    # /[building_name]/floors/[floor_number]
+    # Example: https://aubouleau.fr/NDC/floors/0
     path("<str:building_name>/floors/<int:floor_number>/", views.floor_detail, name="floor_detail"),
-    # /[building_id]/rooms
-    # Example: https://aubouleau.fr/ndc/rooms
+    # /[building_name]/rooms
+    # Example: https://aubouleau.fr/NDC/rooms
     path("<str:building_name>/rooms/", views.rooms, name="rooms"),
-    # /[building_id]/rooms/[room_id]
-    # Example: https://aubouleau.fr/ndc/rooms/l012
+    # /[building_name]/rooms/[room_number]
+    # Example: https://aubouleau.fr/NDC/rooms/L012
     path("<str:building_name>/rooms/<str:room_number>/", views.room_detail, name="room-detail"),
 ]
