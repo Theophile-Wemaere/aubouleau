f805fcf41208faae97b1cd96a40720aeb1b47afc
Quentin LAURENT <quentin.laurent.sup@gmail.com>
2024-06-04 19:23:44 +0200

    added equipment and equipment types
    It is now possible to browse the different pieces of equipment registered in the application.
    As of now, it's prossible to:
    - Browse all the equipment of a building
    - Browse all the equipment of a floor

--- aubouleau_web/admin.py
+++ aubouleau_web/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Building, Floor, Room, TimeSlot
+from .models import Building, Floor, Room, TimeSlot, EquipmentType, Equipment
 
 admin.site.register(Building)
 admin.site.register(Floor)
 admin.site.register(Room)
 admin.site.register(TimeSlot)
+admin.site.register(EquipmentType)
+admin.site.register(Equipment)
