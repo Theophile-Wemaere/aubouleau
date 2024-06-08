60c7289a81c489e7e189c38a4e208f8f8065bc5d
Quentin LAURENT <quentin.laurent.sup@gmail.com>
2024-06-08 17:44:36 +0200

    updated Docker Compose deployment
    The superuser created is now automatically added to the "Administrators" group on startup.
    It's now possible to deploy the stack using existing data saved in the aubouleau-data named volume.
    To do so, set the INITIAL_DEPLOY variable to 'false' in the docker-compose.yml file.

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -8,18 +8,29 @@ printenv > /var/spool/cron/crontabs/root
 service cron start
 
 python manage.py collectstatic --noinput
-python manage.py flush --no-input
-python manage.py makemigrations aubouleau_web
-python manage.py migrate
-python manage.py createsuperuser --noinput
-python manage.py loaddata initial
+
+# For the very first deploy of the app, load initial data
+if [ "$INITIAL_DEPLOY" = true ]
+then
+  python manage.py flush --no-input
+  python manage.py makemigrations aubouleau_web
+  python manage.py migrate
+  python manage.py createsuperuser --noinput
+  python manage.py loaddata initial
+  python manage.py shell < /app/aubouleau_web/add_admin_user_to_administrators_group.py
+else
+  echo "Skipping loading of initial data because INITIAL_DEPLOY was set to false."
+  python manage.py makemigrations aubouleau_web
+  python manage.py migrate
+fi
+
 # Adds the cronjob to the crontab and stores the result in the variable below
 crontab_add_output=$(python manage.py crontab add)
 echo "$crontab_add_output"
 # The crontab hash is extracted using this (not so great) hack
-cronjob_hash=$(echo $crontab_add_output | cut -d '(' -f2 | cut -d ')' -f1)
+cronjob_hash=$(echo "$crontab_add_output" | cut -d '(' -f2 | cut -d ')' -f1)
 # We then manually start the cronjob once
 echo "Starting cronjob $cronjob_hash..."
-python manage.py crontab run $cronjob_hash
+python manage.py crontab run "$cronjob_hash"
 
 exec "$@"
